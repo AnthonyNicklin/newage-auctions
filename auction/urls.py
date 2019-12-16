e70020b21f8eb5ef68c081fc6721e89c764518e0
Anthony <anthonynicklin01@gmail.com>
2019-12-16 21:10:39 +0000

    Correct url patterns

--- auction/urls.py
+++ auction/urls.py
@@ -1,7 +1,11 @@
-# from django.conf.urls import url
+from django.conf.urls import url
 
-# from .views import index
+from .views import all_auctions, auction, bid, all_lot_items, lot
 
-# urlpatterns = [
-#     url(r'^$', index, name=index),
-# ]
+urlpatterns = [
+    url(r'^$', all_auctions, name='all_auctions'),
+    url(r'^(?P<auction_id>\d+)/$', auction, name='auction'),
+    url(r'^(?P<auction_id>\d+)/bid$', bid, name='bid'),
+    url(r'^lots', all_lot_items, name='all_lot_items'),
+    url(r'^lot/(?P<lot_id>\d+)/$', lot, name='lot'),
+]
