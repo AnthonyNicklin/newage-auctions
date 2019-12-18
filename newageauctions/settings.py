45d7e0001a87c3e2d290558e4a2cc8dd8e2ea69e
Anthony <anthonynicklin01@gmail.com>
2019-12-18 21:29:09 +0000

    Travis testing

--- newageauctions/settings.py
+++ newageauctions/settings.py
@@ -90,16 +90,14 @@ WSGI_APPLICATION = 'newageauctions.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-if development:
-        DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+#
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))}
-    
+}
+#
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
