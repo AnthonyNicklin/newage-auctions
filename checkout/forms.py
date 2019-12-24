a5e9b0a54809ed4f091c900dda16cd9adbed471f
Anthony <anthonynicklin01@gmail.com>
2019-12-24 11:05:08 +0000

    Add month 12

--- checkout/forms.py
+++ checkout/forms.py
@@ -4,7 +4,7 @@ from .models import Order
 
 class MakePaymentForm(forms.Form):
 
-    MONTH_CHOICES = [(i, i) for i in range(1, 12)]
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
     YEAR_CHOICES = [(i, i) for i in range(2019, 2049)]
 
     credit_card_number = forms.CharField(label='Credit Card Number', required=False)
