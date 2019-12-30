4b818f1f3f89c8fbe84b8d6c0218bcf3cee2dac4
Anthony <anthonynicklin01@gmail.com>
2019-12-30 17:43:48 +0000

    Refactored winning bid and expired auction into one task

--- auction/tasks.py
+++ auction/tasks.py
@@ -1,13 +1,9 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
-from django.contrib.auth.models import User
 from django.utils import timezone
 
 from celery import shared_task 
 from celery.utils.log import get_task_logger
 
 from auction.models import Auction, Bid
-from cart.views import add_to_cart
 
 
 logger = get_task_logger(__name__)
@@ -21,37 +17,20 @@ def task_set_auction_to_expire():
     auctions = Auction.objects.all()
 
     for auction in auctions:
-        if auction.expired != True and auction.time_ending < timezone.now():
-            auction.expired = True
-            auction.save()
-            logger.info("Expired set")
-        else:
-            logger.info("Auction not expired, aborted function")
-            return
+        if auction.expired != True: 
+            if auction.time_ending < timezone.now():
+                auction.expired = True
+                bid = Bid.objects.filter(auction=auction.pk).order_by('-bid_time')
+                if bid:                                 
+                    latest_bid = bid[0]
+                    auction.winner = latest_bid.user
+                    auction.save()
+                    logger.info("Expired and winner set.")
+                else:
+                    logger.info("Bid not found")
+            else:
+                logger.info("Auction not expired, aborted function")
+                return
 
 
-# @shared_task(name='auction.tasks.task_set_winner_of_auction')
-# def task_set_winner_of_auction():
-#     """ 
-#     If auction.expire is True find the winning 
-#     bid and bind to the user who won 
-#     """
-#     auctions = Auction.objects.all()
-#     user = User.objects.get(pk=request.user.id)
-
-#     for auction in auctions:
-#         if auction.expired:
-#             bid = Bid.objects.filter(auction=auction.pk).order_by('-bid_time')
-#             if bid:                                 
-#                 latest_bid = bid[0]
-#                 if latest_bid.user_id == user.id:
-#                     add_to_cart(request, auction.pk)
-#                     logger.info("Winner found and added to cart")
-#                 else:
-#                     logger.info("No user found for winning bid")
-#             else:
-#                 logger.info("Bid not found")
-#                 return
-#         else:
-#             logger.info("Auction not expired")
-#             return
+    
