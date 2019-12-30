2f7e193122a7c36001548c37ba3065e4aee01a92
Anthony <anthonynicklin01@gmail.com>
2019-12-30 17:47:46 +0000

    Removed add to cart and adjust cart functions

--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import view_cart, add_to_cart, adjust_cart
+from .views import view_cart, remove_from_cart
 
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
-    path('add/<int:id>/', add_to_cart, name='add_to_cart'),
-    path('adjust/<int:auction_id>/', adjust_cart, name='adjust_cart'),
+    path('remove_from_cart/<int:auction_id>/', remove_from_cart, name='remove_from_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,68 +1,42 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 
-from auction.models import Auction, Bid
+from auction.models import Auction
 
 
 @login_required
 def view_cart(request):
     """A View that renders the cart contents page"""
 
+    user = auth.get_user(request)
     auctions = Auction.objects.all()
-    user = User.objects.get(pk=request.user.id)
+    cart = []
 
     for auction in auctions:
-        if auction.expired:
-            if auction.winner == user:
-                add_to_cart(request, auction.pk)
-            else:
-                return render(request, "cart.html")
-        else:
-            return render(request, 'cart.html')
-    return render(request, "cart.html")
+        if user == auction.winner and auction.paid == False:
+            cart.append({'auction': auction})
+
+    context = {
+        'cart': cart
+    }
+    return render(request, "cart.html", context)
 
 
-def add_to_cart(request, id):
-    """Add a quantity of the specified product to the cart"""
-    
-    quantity = 1
-    cart = request.session.get('cart', {})
-    if id in cart:
-        messages.info(request, 'You already have this item in your cart')     
+@login_required
+def remove_from_cart(request, auction_id):
+    """ Remove auction from cart """
+
+    auction = get_object_or_404(Auction, pk=auction_id)
+    user_default = get_object_or_404(User, pk=1)
+
+    if auction:
+        auction.winner = user_default
+        auction.save()
+        messages.success(request, '{0} was removed from the cart'.format(auction_id))
+        return redirect('view_cart')
     else:
-        cart[id] = cart.get(id, quantity)
-
-    request.session['cart'] = cart
-    return redirect(reverse('index'))
-
-
-def adjust_cart(request, id):
-    """
-    Adjust the quantity of the specified product to the specified
-    amount
-    """
-    quantity = 1
-    cart = request.session.get('cart', {})
-
-    if quantity > 0:
-        cart[id] = quantity
-    else:
-        cart.pop(id)
-    
-    request.session['cart'] = cart
-    return redirect(reverse('view_cart'))
-
-
-def remove_from_cart(request, id):
-    """ Remove items from cart """
-
-    auction = get_object_or_404(Auction, pk=id)
-    cart = request.session.get('cart', {})
-
-    del cart[id]
-    request.session['cart'] = cart
-    messages.success(request, '{0} was removed from the cart'.format(auction.lot.name))
-    return redirect('view_cart')
+        messages.error(request, 'Sorry we are unable remove this form your cart')
+        return redirect('view_cart')
