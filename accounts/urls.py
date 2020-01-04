e61abd8e2cadc3377eca07bb1b8f9ef5a1752c6c
Anthony <anthonynicklin01@gmail.com>
2020-01-04 16:33:32 +0000

    Tidy up

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
-from accounts.views import logout, login, registration, user_profile
+
+from .views import logout, login, registration, user_profile, edit_user_profile
 from accounts import urls_reset
 
 urlpatterns = [
@@ -7,5 +8,6 @@ urlpatterns = [
     path('logout/', logout, name='logout'),
     path('register/', registration, name='registration'),
     path('profile/', user_profile, name='profile'),
-    path('password_reset/', include(urls_reset))
+    path('password_reset/', include(urls_reset)),
+    path('edit_profile/', edit_user_profile, name='edit_profile')
 ]
