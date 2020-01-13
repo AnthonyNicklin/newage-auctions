f59479d3e7bcd71dea6c840f64797b2bfa950d9a
Anthony <anthonynicklin01@gmail.com>
2020-01-13 18:40:24 +0000

    Pagination added to search results

--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from auction.models import Lot
 
@@ -11,7 +12,7 @@ def keyword_search(request):
     if request.method == 'GET':
         keyword = request.GET.get('q')
 
-        current_items = []
+        keyword_obj = []
 
         if keyword is None or keyword == "":
             messages.error(request, 'Please enter in a keyword to search')
@@ -21,8 +22,21 @@ def keyword_search(request):
             keyword_lookup = Lot.objects.filter(Q(name__icontains=keyword) | \
                 Q(description__icontains=keyword))
             for item in keyword_lookup:
-                current_items.append(item)
-        
+                keyword_obj.append(item)
+            
+            paginator = Paginator(keyword_obj, 10)
+
+            page = request.GET.get('page')
+
+            try:
+                current_items = paginator.page(page)
+            except PageNotAnInteger:
+                # If the page is not an integer, deliver first page.
+                current_items = paginator.page(1)
+            except EmptyPage:
+                # If page is not an integer, deliver first page.
+                current_items = paginator.page(paginator.num_pages)
+
             return render(request, 'search.html', {'current_items': current_items})
     else:
         return redirect('all_auctions')
