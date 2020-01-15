59c136f4e5e765471f9747cacffa962ceea5b643
Anthony <anthonynicklin01@gmail.com>
2020-01-15 18:47:18 +0000

    More tests writen for auction app

--- auction/testing/test_models.py
+++ auction/testing/test_models.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from datetime import datetime
 
-from auction.models import Lot
+from auction.models import Lot, Auction, Bid
 
 
 class LotModelTest(TestCase):
+    """ Tests fot the Lot model """
     @classmethod
     # Set up non-modified objects used by all test methods
     def setUpTestData(cls):
@@ -41,4 +44,99 @@ class LotModelTest(TestCase):
         self.assertEquals(field_label, 'age')
 
     
+class AuctionModelTest(TestCase):
+    """ Tests for the Auction model """
+    @classmethod
+    # Set up non-modified objects used by all test methods
+    def setUpTestData(cls):
+        Lot.objects.create(id=1, name='Tommy Gun', origin='USA', 
+            description="Gun used by gangsters in the 50's", age=50, category='military')
+        user = User.objects.create_user(
+            username='John12', email='john.smith@gmail.com', password='akdf34983!',
+            first_name='John', last_name='Smith'
+        )
+        user.save()
+        lot_obj = Lot.objects.get(id=1)
+        user_obj = User.objects.get(id=1)
+        Auction.objects.create(id=1, lot=lot_obj, description='Testing model',
+            number_of_bids=3, time_starting=datetime.now(),
+            time_ending="2020-02-02 16:30", expired=False,
+            winner=user_obj, winning_bid=40.00, paid=False)
 
+    def test_lot_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('lot').verbose_name
+        self.assertEquals(field_label, 'lot')
+    
+    def test_description_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('description').verbose_name
+        self.assertEquals(field_label, 'description')
+
+    def test_number_of_bids_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('number_of_bids').verbose_name
+        self.assertEquals(field_label, 'number of bids')
+
+    def test_time_starting_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('time_starting').verbose_name
+        self.assertEquals(field_label, 'time starting')
+
+    def test_time_ending_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('time_ending').verbose_name
+        self.assertEquals(field_label, 'time ending')
+
+    def test_expired_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('expired').verbose_name
+        self.assertEquals(field_label, 'expired')
+        
+    def test_winner_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('winner').verbose_name
+        self.assertEquals(field_label, 'winner')
+
+    def test_winning_bid_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('winning_bid').verbose_name
+        self.assertEquals(field_label, 'winning bid')
+
+    def test_paid_label(self):
+        auction_obj = Auction.objects.get(id=1)
+        field_label = auction_obj._meta.get_field('paid').verbose_name
+        self.assertEquals(field_label, 'paid')
+        
+
+class BidModelTest(TestCase):
+    """ Tests for the Bid model """
+    @classmethod
+    # Set up non-modified objects used by all test methods
+    def setUpTestData(cls):
+        Lot.objects.create(id=1, name='Tommy Gun', origin='USA', 
+            description="Gun used by gangsters in the 50's", age=50, category='military')
+        user = User.objects.create_user(
+            username='John12', email='john.smith@gmail.com', password='akdf34983!',
+            first_name='John', last_name='Smith'
+        )
+        user.save()
+        lot_obj = Lot.objects.get(id=1)
+        user_obj = User.objects.get(id=1)
+        Auction.objects.create(id=1, lot=lot_obj, description='Testing model',
+            number_of_bids=3, time_starting=datetime.now(),
+            time_ending="2020-02-02 16:30", expired=False,
+            winner=user_obj, winning_bid=40.00, paid=False)
+        auction_obj = Auction.objects.get(id=1)
+        Bid.objects.create(id=1, auction=auction_obj, user=user_obj, bid_amount=9999999.99,
+            bid_time=datetime.now())
+
+    def test_bid_amount_label(self):
+        bid_obj = Bid.objects.get(id=1)
+        field_label = bid_obj._meta.get_field('bid_amount').verbose_name
+        self.assertEquals(field_label, 'bid amount')
+
+    def test_bid_time_label(self):
+        bid_obj = Bid.objects.get(id=1)
+        field_label = bid_obj._meta.get_field('bid_time').verbose_name
+        self.assertEquals(field_label, 'bid time')
--- auction/testing/test_views.py
+++ auction/testing/test_views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
@@ -13,14 +13,21 @@ class LotViewTest(TestCase):
         lot = Lot.objects.create(id=1, name='Tommy Gun', origin='USA', 
             description="Gun used by gangsters in the 50's", age=50, category='military')
         lot.save()
+        lot_two = Lot.objects.create(id=2, name='German Bayonet', origin='German', 
+            description="Bayonet used in the battle of Berlin", age=75, category='weapons')
+        lot_two.save()
 
         user = User.objects.create_user(username='Peter', email='pete.polo@gmail.com', password='addkj3245!')
         user.save()
 
         auction = Auction.objects.create(lot=lot, description='Greek Status of the God of War', 
-            number_of_bids=5, time_starting='2020-01-01 16:10:10', time_ending='2020-01-15 16:10:10', 
+            number_of_bids=5, time_starting='2020-01-01 16:10:10', time_ending='2030-01-15 16:10:10', 
             expired=False, winner=user, winning_bid=500.00, paid=False)
+        auction_expired = Auction.objects.create(lot=lot_two, description='German Bayonet', 
+            number_of_bids=4, time_starting=datetime.now(), time_ending='2020-01-15 16:10:10', 
+            expired=False, winner=user, winning_bid=80.00, paid=False)
         auction.save()
+        auction_expired.save()
 
 
     def test_category_view_returns_correct_template(self):
@@ -61,3 +68,9 @@ class LotViewTest(TestCase):
         self.assertTemplateUsed(response, 'auction.html')
 
 
+    def test_auction_expired_returns_correct_template(self):
+        auction = Auction.objects.get(id=2)
+
+        response = self.client.get('/auctions/{0}/'.format(auction.id), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'auction_expired.html')
