13f8cf3a2dff3351fe9002041d8c3886c36b7274
Anthony <anthonynicklin01@gmail.com>
2020-01-19 13:47:46 +0000

    Tidy up urls

--- auction/urls.py
+++ auction/urls.py
@@ -5,8 +5,8 @@ from .views import all_auctions, auction, bid, all_lot_items, lot, lot_sold
 urlpatterns = [
     path('', all_auctions, name='all_auctions'),
     path('<int:auction_id>/', auction, name='auction'),
-    path('<int:auction_id>/bid', bid, name='bid'),
-    path('lots', all_lot_items, name='all_lot_items'),
+    path('<int:auction_id>/bid/', bid, name='bid'),
+    path('lots/', all_lot_items, name='all_lot_items'),
     path('lot/<int:lot_id>/', lot, name='lot'),
     path('lot_sold/<int:auction_id>/', lot_sold, name='lot_sold'),
 ]
