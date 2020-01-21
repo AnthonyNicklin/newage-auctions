ce1ce4fffe80f3948d9fa9f94b49c5d537e94dfe
Anthony <anthonynicklin01@gmail.com>
2020-01-21 21:08:37 +0000

    Heroku debug off

--- newageauctions/settings.py
+++ newageauctions/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG', False)
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = [os.environ.get('HOSTNAME'),
                 '127.0.0.1']
