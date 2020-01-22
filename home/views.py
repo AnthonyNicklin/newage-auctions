c2b4d1a36dddc114122b4cbff5188b8ef472d637
Anthony <anthonynicklin01@gmail.com>
2020-01-22 21:17:00 +0000

    Working on styling

--- home/views.py
+++ home/views.py
@@ -2,25 +2,13 @@ from django.shortcuts import render
 
 from auction.models import Lot
 
+
 def index(request):
-    """ Return render index page """
+    """ Return index page with feature lots """
 
     featured_lots = Lot.objects.filter(featured=True)[0:6]
-    military = Lot.objects.filter(category='military')[0:6]
-    cultural = Lot.objects.filter(category='cultural')[0:6]
-    weapons = Lot.objects.filter(category='weapons')[0:6]
-    real_estate = Lot.objects.filter(category='real estate')[0:6]
 
-    context = {
-        'featured_lots': featured_lots,
-        'military': military,
-        'cultural': cultural,
-        'weapons': weapons,
-        'real_estate': real_estate
-
-    }
-
-    return render(request, 'index.html', context)
+    return render(request, 'index.html', {'featured_lots': featured_lots})
 
 
 def not_found(request):
