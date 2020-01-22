3e596bda0d04f113d2e7b97b401dea1ca2cb782e
Anthony <anthonynicklin01@gmail.com>
2020-01-22 21:15:53 +0000

    Bid read-only labels

--- auction/admin.py
+++ auction/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 
 from .models import Auction, Lot, Bid
 
+
+class BidAdmin(admin.ModelAdmin):
+
+    readonly_fields = (
+        'user',
+        'auction',
+        'bid_amount', 
+        'bid_time',
+    )
+    
+
 admin.site.register(Auction)
 admin.site.register(Lot)
-admin.site.register(Bid)
+admin.site.register(Bid, BidAdmin)
